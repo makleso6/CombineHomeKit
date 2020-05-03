388ef5ea58571e986b3c846de943c0f1476fecac
Maxim Kolesnik <makleso6@gmail.com>
2020-05-03 20:37:26 +0300

    add publisher in extension of HMHomeManager

--- Sources/CombineHomeKit/HMHomeManager+Publisher.swift
+++ Sources/CombineHomeKit/HMHomeManager+Publisher.swift
@@ -4,4 +4,10 @@ import Combine
 
 extension HMHomeManager {
     
+    /// Returns ```HMHomeManager``` wrapper.
+    /// Note: ```HMHomeManager``` instance does not keep strong reference to ```HMHomeManagerPublisher``` instance.
+    ///
+    public var publisher: HMHomeManagerPublisher {
+        return HMHomeManagerPublisher(homeManager: self)
+    }
 }
